632523732794b4b190290dabc14373e45c3b41fe
HeroBuxx <herobuxx@gmail.com>
2022-07-24 16:13:07 +0000

    Revert "init: set build_tags to release-keys"
    This reverts commit f877ca3c900c15e9be7b11db1373c210984ed087.
    
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1256,7 +1256,6 @@ static void SetSafetyNetProps() {
     InitPropertySet("ro.boot.verifiedbootstate", "green");
     InitPropertySet("ro.boot.veritymode", "enforcing");
     InitPropertySet("ro.boot.vbmeta.device_state", "locked");
-    InitPropertySet("ro.build.tags", "release-keys");
 }
 
 void PropertyInit() {
